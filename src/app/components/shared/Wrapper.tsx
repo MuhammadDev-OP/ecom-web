005151ea9d30ec5ac0e56c9506ce65dd5d2f73e9
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-05-30 17:04:34 +0500

    Laptop commit

--- src/app/components/shared/Wrapper.tsx
+++ src/app/components/shared/Wrapper.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactNode } from 'react'
 
 const Wrapper:FC<{children: React.ReactNode}> = ({children}) => {
   return (
-    <div className='max-w-screen-2xl mx-auto'>
+    <div className='max-w-screen-2xl mx-auto px-4'>
         {children}
         </div>
   )
---
+++ src/app/components/widgets/Hero.tsx
@@ -0,0 +1,21 @@
+import Wrapper from "../shared/Wrapper";
+
+const Hero = () => {
+  return (
+    <>
+    <section className="bg-lime-100">
+      <Wrapper>
+        <div className="max-w-screen-2xl">
+          <h1 className="font-bold text-lg bg-lime-200">
+            An Industrial Take on StreetWater
+          </h1>
+
+
+        </div>
+      </Wrapper>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
