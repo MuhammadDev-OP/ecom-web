849576abd7eac36cd6d9af9aa2d32d2d5dc18c9d
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-06-05 09:29:18 +0500

    Edits Sanity Linking

--- src/app/components/widgets/Featured.tsx
+++ src/app/components/widgets/Featured.tsx
@@ -6,32 +6,11 @@ export default function Featured() {
     <>
       <section>
         <Wrapper>
-            {/* <div className="flex space-x-3 -mt-10 items-center max-w-screen-xl mx-auto">
-              <Image
-                src={"/Featured1.webp"}
-                alt={"logo"}
-                width={100}
-                height={100}
-              />
-              <Image
-                src={"/Featured2.webp"}
-                alt={"logo"}
-                width={100}
-                height={100}
-              />
-              <Image
-                src={"/Featured3.webp"}
-                alt={"logo"}
-                width={100}
-                height={100}
-              />
-              <Image
-                src={"/Featured4.webp"}
-                alt={"logo"}
-                width={100}
-                height={100}
-              />
-            </div> */}
+          
+
+
+
+
         </Wrapper>
       </section>
     </>
