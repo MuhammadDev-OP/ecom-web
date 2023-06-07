06c220c4f8273262965b6934590b3548e9b2d728
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-06-07 09:47:41 +0500

    sdsas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={sora.className}>
         <Header />
+        
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 import Hero from "./components/widgets/Hero";
 import Featured from "./components/widgets/Featured";
 import slider from "./components/widgets/Slider";
+import Footer from "./components/widgets/Footer";
 
 export default async function Home() {
   return (
     <>
-    <main>
-    <Hero />
-    <Featured />
-    
-    </main>
-   </>
+      <main>
+        <Hero />
+        <Featured />
+        <Footer />
+      </main>
+    </>
   );
 }
