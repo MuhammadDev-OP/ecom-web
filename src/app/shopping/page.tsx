c8dffe5ec912fe363ef08729412b1fc363e4b4cd
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-06-09 16:22:29 +0500

    edit

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,22 +1,31 @@
-import Wrapper from "../components/shared/Wrapper";
-import Footer from "../components/widgets/Footer";
-import { client } from "../lib/sanityClient";
+"use client";
+import Image from "next/image";
+import { BsCart } from "react-icons/Bs";
 import { Image as IImage } from "sanity";
 
-export const getProductData = async () => {
+import { client } from "@/app/lib/sanityClient";
+import { data } from "autoprefixer";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Wrapper from "../components/shared/Wrapper";
+import { urlForImage } from "../../../sanity/lib/image";
+import Footer from "../components/widgets/Footer";
+import { product } from "../../../sanity/product";
+
+export const getPD = async () => {
   const res = await client.fetch(`*[_type=="product"]{
-        price,
-        image,
-        _id,
-        title,
-        category -> {
-          name
-        }
-      }`);
+          price,
+          image,
+          _id,
+          title,
+          category -> {
+            name
+          }
+        }`);
   return res;
 };
 
-interface IProduct {
+interface GProduct {
   title: string;
   _id: string;
   description: string;
@@ -27,18 +36,27 @@ interface IProduct {
   };
 }
 
-
-
-
-
-export default function shopping() {
+export default async function shopping() {
+  const data: GProduct[] = await getPD();
   return (
     <>
       <Wrapper>
         <div className="items-center max-w-screen-xl mx-auto mt-8">
-            <p>Hello</p>
-
-
+          {data.map((item) => (
+            <div>
+              <div className="grid grid-cols-[repeat(2, auto)] justify-center gap-x-10 ">
+                <Image
+                  width={200}
+                  height={100}
+                  className="max-h-[200px] object-cover object-top"
+                  src={urlForImage(item.image).url()}
+                  alt={"product"}
+                />
+                <h2>{item.title}</h2>
+                <h3>${item.price}</h3>
+              </div>
+            </div>
+          ))}
         </div>
       </Wrapper>
       <Footer />
